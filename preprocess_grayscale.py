ac357ca5d9927d997b23358bd553170045a20241
dashan124 <dashanjotsingh961999@gmail.com>
2020-06-07 21:49:18 +0530

    augment

--- preprocess_grayscale.py
+++ preprocess_grayscale.py
@@ -12,8 +12,8 @@ from PIL import Image
 import cv2
 import os 
 
-path_1="/home/dashan/Desktop/Major_project/retina_dataset/dataset"
-path_2="/home/dashan/Desktop/Major_project/modified_data"
+path_1="/home/dashan/Desktop/augmented_data"
+path_2="/home/dashan/Desktop/modified_data"
 
 p1=path_1+"/1_normal"
 path=p1
@@ -21,15 +21,18 @@ IMG_SIZE=300
 #first 300 images of normal eye
 # def process_normal_eye_images():
 files = [f for f in listdir(path) if isfile(join(path,f))] 
-
+# print(len(files))
+# print('bc')
 for image in files:
     img = cv2.imread(os.path.join(path,image))
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     # gray = cv2.resize(gray, (200,200)) 
     gr = cv2.resize(gray,(IMG_SIZE,IMG_SIZE))
+    print("lol1")
     dstPath = join(path_2+"/1_normal",image)
+    print("lol2")
     cv2.imwrite(dstPath,gr)
-
+    print("lol3")
 '''
 then 100 images of cataract eye
 '''
@@ -46,25 +49,25 @@ for image in files:
 then 101 images of glucoma eye
 '''
 
-p3=path_1+"/2_glaucoma"
-path=p3
-files = [f for f in listdir(path) if isfile(join(path,f))] 
-for image in files:
-    img = cv2.imread(os.path.join(path,image))
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    gr = cv2.resize(gray,(IMG_SIZE,IMG_SIZE))
-    dstPath = join(path_2+"/2_glaucoma",image)
-    cv2.imwrite(dstPath,gr)
-'''
-100 images of ratina disease
-'''
+# p3=path_1+"/2_glaucoma"
+# path=p3
+# files = [f for f in listdir(path) if isfile(join(path,f))] 
+# for image in files:
+#     img = cv2.imread(os.path.join(path,image))
+#     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#     gr = cv2.resize(gray,(IMG_SIZE,IMG_SIZE))
+#     dstPath = join(path_2+"/2_glaucoma",image)
+#     cv2.imwrite(dstPath,gr)
+# '''
+# 100 images of ratina disease
+# '''
 
-p4=path_1+"/3_retina_disease"
-path=p4
-files = [f for f in listdir(path) if isfile(join(path,f))] 
-for image in files:
-    img = cv2.imread(os.path.join(path,image))
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    gr = cv2.resize(gray,(IMG_SIZE,IMG_SIZE))
-    dstPath = join(path_2+"/3_retina_disease",image)
-    cv2.imwrite(dstPath,gr)
+# p4=path_1+"/3_retina_disease"
+# path=p4
+# files = [f for f in listdir(path) if isfile(join(path,f))] 
+# for image in files:
+#     img = cv2.imread(os.path.join(path,image))
+#     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#     gr = cv2.resize(gray,(IMG_SIZE,IMG_SIZE))
+#     dstPath = join(path_2+"/3_retina_disease",image)
+#     cv2.imwrite(dstPath,gr)
